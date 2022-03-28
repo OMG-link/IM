08e9b6e3cd2bb6a0ff89352ba9beb19ec7e616ed
OMG-link <50469469+OMG-link@users.noreply.github.com>
2022-03-28 15:04:54 +0800

    更正了计量单位的错误

--- src/Core/src/mutils/FileUtils.java
+++ src/Core/src/mutils/FileUtils.java
@@ -4,14 +4,14 @@ public class FileUtils {
     public static String sizeToString(long size){
         if(size<1000L){
             return String.format("%dB",size);
-        }else if(size<(1L<<10)*1000L){
-            return String.format("%.2fKB",(double)size/(1L<<10));
-        }else if(size<(1L<<20)*1000L){
-            return String.format("%.2fMB",(double)size/(1L<<20));
-        }else if(size<(1L<<30)*1000L){
-            return String.format("%.2fGB",(double)size/(1L<<30));
+        }else if(size<(long)1e6){
+            return String.format("%.2fKB",(double)size/1e3);
+        }else if(size<(long)1e9){
+            return String.format("%.2fMB",(double)size/1e6);
+        }else if(size<(long)1e12){
+            return String.format("%.2fGB",(double)size/1e9);
         }else{
-            return String.format("%.2fTB",(double)size/(1L<<40));
+            return String.format("%.2fTB",(double)size/1e12);
         }
     }
 }
